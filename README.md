0eae63ee70ff36ffd7864e24116913f8233f33fc
Zainan Victor Zhou <zzn@zzn.im>
2023-08-01 14:56:01 -0700

    Update documentation

--- README.md
+++ README.md
@@ -1,29 +1,6 @@
-# Donkey Decay NFT
+# D3Bridge NFT
 [![CI](https://github.com/xinbenlv/donkey-decay-club/actions/workflows/ci.yml/badge.svg)](https://github.com/xinbenlv/donkey-decay-club/actions/workflows/ci.yml)
 
-## Price time decay
+## Current Deployment
 
-
-$P = P_{initial} \left(\frac{P_{final}}{P_{initial}}\right)^{\frac{B}{B_{total}}}$
-
-$P = \frac{P_{initial} \left(\frac{P_{final}}{P_{initial}}\right)^{\frac{B \cdot s}{B_{total} \cdot s}}}{s}$
-
-## Installing dependencies
-
-```
-npm install
-```
-
-## Testing the contract
-
-```
-npm test
-```
-
-## Deploying the contract
-
-You can target any network from your Hardhat config using:
-
-```
-npx hardhat run --network <network-name> scripts/deploy.ts
-```
+- Goerle v0.0.1 [0x9765eFf10c752DB8Ef81fe655cEB1543AbE7b16D](https://goerli.etherscan.io/address/0x9765eFf10c752DB8Ef81fe655cEB1543AbE7b16D#writeContract)
--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -4,25 +4,30 @@ import { deployByName } from "../utils/deployUtil";
 
 const WAIT_FOR_BLOCK = 3;
 
-task("d3bridge-deploy", "Deploy")
+task("d3bridge-nft-deploy", "Deploy")
     .setAction(async function (taskArguments: TaskArguments, { ethers, run }) {
-        const { contractName, salt, create2Factory } = taskArguments;
+        const contractName = "D3BridgeNFT";
         const signers = await ethers.getSigners();
         const signer = signers[0];
-        const contractFactory = await ethers.getContractFactory(contractName);
-        await signer.sendTransaction({ 
-            to: create2Factory.address, 
-            data: contractFactory.bytecode,
-        });
+        const {contract, tx} = await deployByName(
+            ethers,
+            contractName,
+            [],
+            signer);
+        
+        console.log(`Contract ${contractName} deployed to ${contract.address}`);
 
+        // wait for a few blocks
+        const WAIT_FOR_BLOCK = 6;
+        console.log(`Waiting for ${WAIT_FOR_BLOCK} blocks...`);
         for (let i = 0; i < WAIT_FOR_BLOCK; i++) {
-            console.log(`Block ${i}...`);
+            console.log(`${i} time: ${new Date().toLocaleTimeString()}`);
             await tx.wait(i);
         }
-
-        console.log(`Done waiting for the confirmation for contract ${contractName} at ${logic.address}`);
+        console.log(`Wait done for ${WAIT_FOR_BLOCK} blocks.`);
+        // verify on etherscan
         await run("verify:verify", {
-            address: logic.address,
-        }).catch(e => console.log(`Failure ${e} when verifying ${contractName} at ${logic.address}`));
-        console.log(`Done verifying ${contractName} at ${logic.address}`);
+            address: contract.address
+        });
+        console.log(`Contract ${contractName} verified at ${contract.address}.`);
     });
