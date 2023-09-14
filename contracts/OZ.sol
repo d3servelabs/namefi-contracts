1ab6b668d1926fa2665f744bdfca7a1742881818
Zainan Victor Zhou <zzn@zzn.im>
2023-09-14 13:46:36 -0700

    Add OZContracts

---
+++ contracts/OZ.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Proprietary
+pragma solidity 0.8.19;
+
+// Import ProxyAdmin
+// Import TransparentUpgradeableProxy
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
