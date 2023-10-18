b8a7a0857e99d364a0e9a9834e04b005d392c7ba
Zainan Victor Zhou <zzn@zzn.im>
2023-10-18 11:47:45 -0700

    Add a way to print public keys of BIP-44 derived wallets

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -3,6 +3,18 @@ import type { TaskArguments } from "hardhat/types";
 import { deployByName } from "../utils/deployUtil";
 
 const WAIT_FOR_BLOCK = 6;
+task("print-signers", "Printer signers")
+    .setAction(async function (taskArguments: TaskArguments, { ethers, run }) {
+        // get signer from hardhat 
+        
+        const node = ethers.utils.HDNode.fromMnemonic(process.env.MNEMONIC as string);
+        
+        for (let i = 0; i < 10; i++) {
+            const path = `m/44'/60'/0'/0/${i}`;
+            const wallet = node.derivePath(path);
+            console.log(`${path}: ${wallet.address}`);
+        }
+    });
 
 task("d3bridge-logic-deploy", "Deploy the logic contract")
     .addParam("logicContractName")
